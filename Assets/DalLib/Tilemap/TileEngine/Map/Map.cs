2c669b55f5bef4995220dfd617fe5a7a1c8f08e8
daleran <daleran@gmail.com>
2018-06-06 12:10:23 +0900

    Imported TMPro, ProGrids, DalLib, and Platformer

---
+++ Assets/DalLib/Tilemap/TileEngine/Map/Map.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace DaleranGames.TileEngine
+{
+
+    public class Map : Singleton<Map>
+    {
+
+        [Header("Map Generation")]
+        public MapTemplate Template;
+
+        [Header("Map Layers")]
+        public Tilemap Terrain;
+        
+
+        // Update is called once per frame
+        void Awake()
+        {
+            GenerateMap();
+        }
+
+        [ContextMenu("Generate Map")]
+        public void GenerateMap()
+        {
+            ClearMap();
+            DataGrid<GameObject> objects;
+            DataGrid < TileBase> tiles;
+            Template.GenerateMap(out tiles,out objects, Vector3Int.zero);
+            Terrain.SetTiles(tiles.Positions, tiles.Values);
+        }
+
+        [ContextMenu("Clear Map")]
+        public void ClearMap()
+        {
+            Terrain?.ClearAllTiles();
+        }
+
+
+    }
+}
